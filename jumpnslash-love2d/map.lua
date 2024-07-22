340cf4d5643bf1cf81b14000083ad9bddeb61a46
HankDeTank05 <henryhankholman@gmail.com>
2024-07-22 15:28:31 -0500

    added more empty functions from pico8 version

--- jumpnslash-love2d/map.lua
+++ jumpnslash-love2d/map.lua
@@ -53,3 +53,46 @@ end
 
 function map_get_current_bottom_bounds()
 end
+
+-- formerly known as "get_scrollability_horizontal()"
+function map_is_horizontal_scrolling()
+end
+
+-- formerly known as "get_scrollability_vertical()"
+function map_is_vertical_scrolling()
+end
+
+function map_get_scroll_left_bounds()
+end
+
+function map_get_scroll_right_bounds()
+end
+
+function map_get_scroll_top_bounds()
+end
+
+function map_get_scroll_bottom_bounds()
+end
+
+-- formerly known as "get_current_map_x()"
+function map_get_current_room_origin_x()
+end
+
+-- formerly known as "get_current_map_y()"
+function map_get_current_room_origin_y()
+end
+
+-- formerly known as "check_for_flag_at_pix(_map_pix_x, _map_pix_y, _flag)"
+function map_check_for_flag_at_world_pos(_world_x, _world_y, _flag)
+end
+
+-- formerly known as "check_for_flag_at_tile(_tile_x, _tile_y, _flag)"
+function map_check_for_flag_at_tile(_tile_x, _tile_y, _flag)
+end
+
+-- formerly known as "get_room_from_pixel(_pixel_x, _pixel_y)"
+function map_get_room_from_world_pos(_world_x, _world_y)
+end
+
+function map_get_room_from_tile(_tile_x, _tile_y)
+end
