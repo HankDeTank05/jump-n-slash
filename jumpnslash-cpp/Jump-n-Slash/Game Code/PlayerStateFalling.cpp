c3a4be65f83f49f01653a01ad29eed28caba56c5
Pinkatie <75712154+Pinkatie@users.noreply.github.com>
2024-10-09 18:11:15 -0500

    Added logic for GetNextState function in PlayerStateJumping and PlayerStateFalling

--- jumpnslash-cpp/Jump-n-Slash/Game Code/PlayerStateFalling.cpp
+++ jumpnslash-cpp/Jump-n-Slash/Game Code/PlayerStateFalling.cpp
@@ -1,5 +1,8 @@
 #include "PlayerStateFalling.h"
 
+#include "PlayerFSM.h"
+#include "PlayerAttorney.h"
+
 PlayerStateFalling::PlayerStateFalling()
 {
 	// do nothing
@@ -34,7 +37,17 @@ const PlayerMoveState* PlayerStateFalling::GetNextState(Player* pPlayer) const
 {
 	const PlayerMoveState* pNextState = this;
 
-	// logic goes here
+	if (PlayerAttorney::State::GetPosDelta(pPlayer).y < 0.0f) {
+		pNextState = &PlayerFSM::jumping; // Not technically possible at the moment
+	}
+	else if (PlayerAttorney::State::GetPosDelta(pPlayer).y == 0.0f) { // Would prefer to check if the player is grounded
+		if (PlayerAttorney::State::GetPosDelta(pPlayer).x != 0.0f) {
+			pNextState = &PlayerFSM::walking;
+		}
+		else { // If this statement is reached, then the player is not moving vertically or horizontally
+			pNextState = &PlayerFSM::idle;
+		}
+	}
 
 	return pNextState;
 }
--- jumpnslash-cpp/Jump-n-Slash/Game Code/PlayerStateJumping.cpp
+++ jumpnslash-cpp/Jump-n-Slash/Game Code/PlayerStateJumping.cpp
@@ -1,5 +1,8 @@
 #include "PlayerStateJumping.h"
 
+#include "PlayerFSM.h"
+#include "PlayerAttorney.h"
+
 PlayerStateJumping::PlayerStateJumping()
 {
 	// do nothing
@@ -34,7 +37,16 @@ const PlayerMoveState* PlayerStateJumping::GetNextState(Player* pPlayer) const
 {
 	const PlayerMoveState* pNextState = this;
 
-	// logic goes here
-
+	if (PlayerAttorney::State::GetPosDelta(pPlayer).y > 0.0f) {
+		pNextState = &PlayerFSM::falling;
+	}
+	else if (PlayerAttorney::State::GetPosDelta(pPlayer).y == 0.0f) { // Would prefer to check if the player is grounded
+		if (PlayerAttorney::State::GetPosDelta(pPlayer).x != 0.0f) {
+			pNextState = &PlayerFSM::walking; // Not technically possible at the moment
+		}
+		else { // If this statement is reached, then the player is not moving vertically or horizontally
+			pNextState = &PlayerFSM::idle; // Not technically possible at the moment
+		}
+	}
 	return pNextState;
 }
