26433c166187064055f9de819e131861b6e909a2
HankDeTank05 <henryhankholman@gmail.com>
2024-10-29 12:07:48 -0500

    Update AlarmObject.cpp

--- jumpnslash-cpp/Jump-n-Slash/Engine Code/AlarmObject.cpp
+++ jumpnslash-cpp/Jump-n-Slash/Engine Code/AlarmObject.cpp
@@ -101,6 +101,7 @@ void AlarmObject::RequestAlarmDeregistration(AlarmID id)
 
 void AlarmObject::TriggerAlarm(AlarmID id)
 {
+	regState[static_cast<int>(id)].regState = RegistrationState::CURRENTLY_DEREGISTERED;
 	switch (id)
 	{
 	case AlarmID::Alarm0:
