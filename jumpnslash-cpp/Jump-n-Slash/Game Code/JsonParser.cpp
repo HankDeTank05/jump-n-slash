9521934332ec6005f0c41a9d203a5a22f3978998
HankDeTank05 <henryhankholman@gmail.com>
2024-12-04 12:08:46 -0600

    BIZARRE if statement bug

--- jumpnslash-cpp/Jump-n-Slash/Game Code/JsonParser.cpp
+++ jumpnslash-cpp/Jump-n-Slash/Game Code/JsonParser.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 // engine includes
 #include "../Engine Code/ConvenienceFunctions.h"
 
 // game includes
+#include "JsonNode.h"
 #include "JsonNodeList.h"
 #include "JsonNodeValue.h"
 
@@ -41,8 +43,9 @@ void JsonParser::ReadJsonFile(std::string path)
 	std::vector<std::pair<int, bool>> braces;
 
 	// kvLines[i].first = line number
-	// kvLines[i].second = (true = sublist, false = value)
-	std::vector<std::pair<int, bool>> kvLines;
+	// kvLines[i].second.first = key
+	// kvLines[i].second.second = (true = sublist, false = value)
+	std::vector<std::pair<int, std::pair<std::string, bool>>> kvLines;
 
 	// step 1: parse the json file for important data, line by line
 	while (std::getline(jsonFile, line))
@@ -50,15 +53,21 @@ void JsonParser::ReadJsonFile(std::string path)
 		// parse a line of the file
 
 		int strIndex = 0;
-		std::string currentChar = line.substr(strIndex, strIndex + 1);
+		std::string currentChar = line.substr(strIndex, 1);
 
 		// step 1.1: check the indent level for a hint on how nested this line is
 		
 		int indentLevel = 0;
-		while (currentChar == "\t")
+		int spaces = 0;
+		while (currentChar == " ")
 		{
-			indentLevel++;
+			spaces++;
+			if (spaces % 4 == 0)
+			{
+				indentLevel++;
+			}
 			strIndex++;
+			currentChar = line.substr(strIndex, 1);
 		}
 		indents.push_back(indentLevel);
 
@@ -78,19 +87,39 @@ void JsonParser::ReadJsonFile(std::string path)
 		size_t colonPos = line.find(":");
 		if (colonPos != std::string::npos)
 		{
+			// get the key on this line
+			size_t keyStartPos = colonPos - 2; // colonPos - 1 should be the index of the closing double-quote
+			int keyLen = 1;
+
+			// sanity check to make sure colons are always preceded by a closing double-quote
+			assert(line.substr(colonPos - 1, 1) == "\""); 
+
+			// iterate backwards until you find the opening quote
+			while (line.substr(keyStartPos - 1, 1) != "\"")
+			{
+				keyStartPos--;
+				keyLen++;
+			}
+
+			// sanity check to make sure keyStartPos is the index of the first letter of the key, not the index of the opening double-quote
+			assert(line.substr(keyStartPos - 1, 1) == "\""); 
+
+			std::string key = line.substr(keyStartPos, keyLen);
+
 			// get the type of the value (either string or sublist)
 			std::string valueChar = line.substr(colonPos + 2, 1);
 
 			// check if value type is string
 			if (valueChar == "\"")
 			{
-				kvLines.push_back(std::pair<int, bool>(lineNum, false));
+				kvLines.push_back(std::pair<int, std::pair<std::string, bool>>(lineNum, std::pair<std::string, bool>(key, false)));
 			}
 			// check if value type is sublist
 			else if (valueChar == "{")
 			{
-				kvLines.push_back(std::pair<int, bool>(lineNum, true));
+				kvLines.push_back(std::pair<int, std::pair<std::string, bool>>(lineNum, std::pair<std::string, bool>(key, true)));
 			}
+			// check if value type is an integer
 			else if (std::stoi(valueChar))
 			{
 				// do nothing yet (but if you get here, you know the value is a number)
@@ -107,7 +136,9 @@ void JsonParser::ReadJsonFile(std::string path)
 	}
 	assert(lineNum == indents.size());
 
-	// step 2: determine max indent level
+	// step 2: create data structure and populate with json data
+
+	// step 2.1: determine max indent level
 
 	int maxIndentLv = 0;
 	for (int i = 0; i < indents.size(); i++)
@@ -115,6 +146,25 @@ void JsonParser::ReadJsonFile(std::string path)
 		if (indents[i] > maxIndentLv)
 		{
 			maxIndentLv = indents[i];
+		}
+	}
+
+	std::queue<std::pair<int, JsonNodeList*>> q;
+	for (int currIndentLv = 1; currIndentLv <= maxIndentLv; currIndentLv++)
+	{
+		for (int i = 0; i < kvLines.size(); i++)
+		{
+			lineNum = kvLines[i].first;
+			bool hasSublist = kvLines[i].second.second;
+			int lineIndent = indents[lineNum];
+			if (lineIndent == currIndentLv && hasSublist == true);
+			{
+				assert(lineIndent == currIndentLv);
+				assert(hasSublist == true);
+				std::string key = kvLines[i].second.first;
+				JsonNodeList* pListNode = new JsonNodeList(key);
+				q.push(std::pair<int, JsonNodeList*>(lineNum, pListNode));
+			}
 		}
 	}
 
