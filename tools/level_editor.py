e48fc5cacc018c59ab8ab47896d1c066c3dab532
HankDeTank05 <henryhankholman@gmail.com>
2025-01-31 08:42:46 -0600

    fixed a bug with gridlines
    fixed a bug where gridlines would always redraw after the mouse button was released, regardless of the user's gridline setting

--- tools/level_editor.py
+++ tools/level_editor.py
@@ -562,10 +562,10 @@ class GuiGridView:
 		# draw an empty square
 		self._DrawSqaureAtGridPos(gridX, gridY)
 
-		# if self.showGridlines == True:
-		# 	# redraw the gridlines above and to the left
-		# 	self._DrawVerticalGridlineAtGridX(gridX)
-		# 	self._DrawHorizontalGridlineAtGridY(gridY)
+		if self.showGridlines == True:
+			# redraw the gridlines above and to the left
+			self._DrawVerticalGridlineAtGridX(gridX)
+			self._DrawHorizontalGridlineAtGridY(gridY)
 
 	'''
 	write a line of tiles to the map data, and draw it on the canvas
@@ -770,7 +770,10 @@ class GuiGridView:
 	def _MouseButtonReleased(self, event: tk.Event) -> None:
 		# print(event)
 		self.mouseButtonReleased = True
-		self._DrawGridlines()
+
+		# redraw gridlines after releasing the mouse button
+		if self.showGridlines == True:
+			self._DrawGridlines()
 
 	def _MouseMoved(self, event: tk.Event) -> None:
 		gridPos: tuple[int, int] = self._ScreenPixelToGridPos(event.x, event.y)
@@ -915,32 +918,32 @@ class GuiTileDetailsPanel:
 		# Tile Name
 		self.w_nameLabel: ttk.Label = ttk.Label(self.w_parentFrame, text="Name:")
 		self.w_nameLabel.grid(column=0, row=0, sticky="W", padx=10, pady=5)
-		self.w_nameValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["name"])
-		self.w_nameValue.grid(column=1, row=0, sticky="W", padx=10, pady=5)
+		# self.w_nameValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["name"])
+		# self.w_nameValue.grid(column=1, row=0, sticky="W", padx=10, pady=5)
 
         # Damage to Player
 		self.w_damagePlayerLabel: ttk.Label = ttk.Label(self.w_parentFrame, text="Damage to Player:")
 		self.w_damagePlayerLabel.grid(column=0, row=1, sticky="W", padx=10, pady=5)
-		self.w_damagePlayerValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["damage_to_player"])
-		self.w_damagePlayerValue.grid(column=1, row=1, sticky="W", padx=10, pady=5)
+		# self.w_damagePlayerValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["damage_to_player"])
+		# self.w_damagePlayerValue.grid(column=1, row=1, sticky="W", padx=10, pady=5)
 
         # Damage to Enemies
 		self.w_damageEnemiesLabel: ttk.Label = ttk.Label(self.w_parentFrame, text="Damage to Enemies:")
 		self.w_damageEnemiesLabel.grid(column=0, row=2, sticky="W", padx=10, pady=5)
-		self.w_damageEnemiesValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["damage_to_enemies"])
-		self.w_damageEnemiesValue.grid(column=1, row=2, sticky="W", padx=10, pady=5)
+		# self.w_damageEnemiesValue: ttk.Label = ttk.Label(self.w_parentFrame, text=self.tile_data["damage_to_enemies"])
+		# self.w_damageEnemiesValue.grid(column=1, row=2, sticky="W", padx=10, pady=5)
 
         # Breakable Status
 		self.w_breakableLabel: ttk.Label = ttk.Label(self.w_parentFrame, text="Breakable:")
 		self.w_breakableLabel.grid(column=0, row=3, sticky="W", padx=10, pady=5)
-		self.w_breakableValue: ttk.Label = ttk.Label(self.w_parentFrame, text="Yes" if self.tile_data["is_breakable"] else "No")
-		self.w_breakableValue.grid(column=1, row=3, sticky="W", padx=10, pady=5)
+		# self.w_breakableValue: ttk.Label = ttk.Label(self.w_parentFrame, text="Yes" if self.tile_data["is_breakable"] else "No")
+		# self.w_breakableValue.grid(column=1, row=3, sticky="W", padx=10, pady=5)
 
         # Solid Status
 		self.w_solidLabel: ttk.Label = ttk.Label(self.w_parentFrame, text="Solid:")
 		self.w_solidLabel.grid(column=0, row=4, sticky="W", padx=10, pady=5)
-		self.w_solidValue: ttk.Label = ttk.Label(self.w_parentFrame, text="Yes" if self.tile_data["is_solid"] else "No")
-		self.w_solidValue.grid(column=1, row=4, sticky="W", padx=10, pady=5)
+		# self.w_solidValue: ttk.Label = ttk.Label(self.w_parentFrame, text="Yes" if self.tile_data["is_solid"] else "No")
+		# self.w_solidValue.grid(column=1, row=4, sticky="W", padx=10, pady=5)
 
 		# TODO: Still need to integrate into GUITilePalette.
 
@@ -949,11 +952,12 @@ class GuiTileDetailsPanel:
 		# NOTE: currently, this is displaying the filename for the brush tile
 		# TODO: we want just the tile name (no "<paletteName>_" prefix, no ".png" suffix) so that we can look for "<tileName>.json" and read the tile's data from there
 		# TODO: we also want a second version of the <tileName> that's not in camel case so that the displayed name looks nice in the GUI
-		self.w_nameValue.config(text=img.name)
+		# self.w_nameValue.config(text=img.name)
 		# self.w_damagePlayerValue.config(text="UPDATED DAMAGE TO PLAYER")
 		# self.w_damageEnemiesValue.config(text="UPDATED DAMAGE TO ENEMIES")
 		# self.w_breakableValue.config(text="UPDATED BREAKABILITY")
 		# self.w_solidValue.config(text="UPDATED SOLIDITY")
+		pass
 
 class GuiTilePalette:
 
