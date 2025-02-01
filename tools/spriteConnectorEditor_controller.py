a961f482883fc21ab130457824d05bdefa9da99c
HankDeTank05 <henryhankholman@gmail.com>
2025-02-01 10:33:20 -0600

    worked some more on the sprite connector editor

--- tools/spriteConnectorEditor_controller.py
+++ tools/spriteConnectorEditor_controller.py
@@ -1,6 +1,10 @@
+# language imports
+import tkinter as tk
+
 # app imports
 from spriteConnectorEditor_model import Model
 from spriteConnectorEditor_view import View
+from spriteConnectorEditor_model import Point
 
 class Controller:
 	
@@ -8,20 +12,37 @@ class Controller:
 		self.model: Model = model
 		self.view: View = view
 
-		# set the buttons for the entity selector
-		entityList: list[str] = self.model.entitySelector.GetEntityList()
-		callback: function = self.SelectEntity
-		self.view.entitySelector.PopulateButtonList(entityList, callback)
+		entityList: list[str] = self.model.GetEntityList()
+		self.view.entitySelector.PopulateButtonList(entityList, self.SelectEntity)
+
+		self.view.ptSelector.SetAddPointCommand(self.AddPoint)
 
 	def SelectEntity(self, entityName: str) -> None:
-		animList: list[str] = self.model.animSelector.GetSelectionOptions(entityName)
-		self.view.animationSelector.PopulateButtonList(animList)
+		self.model.SelectEntity(entityName)
+		animList: list[str] = self.model.GetAnimList()
+		self.view.animSelector.PopulateButtonList(animList, self.SelectAnim) # populate the animation selector
+		self.view.sprSelector.DepopulateButtonList() # depopulate the sprite selector
+		self.view.ptSelector.DepopulateItemList() # depopulate the point selector
+		# TODO: clear the point editor
 
 	def SelectAnim(self, animName: str) -> None:
+		self.model.SelectAnim(animName)
+		sprNameList: list[str] = self.model.GetSprNameList()
+		sprImgList: list[tk.PhotoImage] = self.model.GetSprImgList()
+		self.view.sprSelector.PopulateButtonList(sprNameList, sprImgList, self.SelectSpr) # populate the sprite selector
+		self.view.ptSelector.DepopulateItemList() # depopulate point selector
+		# TODO: depopulate the point editor
+
+	def SelectSpr(self, sprFilename: str) -> None:
+		self.model.SelectSpr(sprFilename)
+		ptList: list[Point] = self.model.GetPtList()
+		self.view.ptSelector.PopulateItemList(ptList) # populate the point list
+		# TODO: populate the point editor in "awaiting point selection" mode
+
+	def SelectPoint(self, ptName: str) -> None:
 		pass
 
-	def SelectSpr(self, sprName: str) -> None:
-		pass
-
-	def SelectPoint(self, pointName: str) -> None:
-		pass
+	def AddPoint(self, ptName: str) -> None:
+		self.model.AddPoint(ptName) # add the point
+		ptList: list[Point] = self.model.GetPtList() # get the new point list
+		self.view.ptSelector.PopulateItemList(ptList) # re-populate the item list now that we've added a new point
--- tools/spriteConnectorEditor_model.py
+++ tools/spriteConnectorEditor_model.py
@@ -1,6 +1,7 @@
 # language imports
 import os.path
 import tkinter as tk
+import json
 
 # game imports
 import jnscommon as jns
@@ -8,66 +9,152 @@ import jnscommon as jns
 class Model:
 	
 	def __init__(self) -> None:
-		# lists
-		self._entityList: list[str] = jns.GetFoldersAtPath(jns.READ_LOCATION_TEXTURES_ENTITIES)
-		self._animList: list[str] = []
 		# selections
 		self._selectedEntity: str = ""
 		self._selectedAnim: str = ""
+		self._selectedSpr: str = ""
+		# lists
+		self._entityList: list[str] = jns.GetFoldersAtPath(jns.READ_LOCATION_TEXTURES_ENTITIES)
+		self._animList: list[str] = []
+		self._sprList: list[str] = []
+		self._ptList: list[Point] = []
+		# dicts
+		self._sprImgDict: dict[str, tk.PhotoImage] = {} # key=filename, value=corresponding image
+		# point data
+		self._pointData: PointData | None = None
 
 	def GetEntityList(self) -> list[str]:
 		return self._entityList
 
 	def GetAnimList(self) -> list[str]:
 		return self._animList
+	
+	def GetSprNameList(self) -> list[str]:
+		return self._sprList
+	
+	def GetSprImgList(self) -> list[tk.PhotoImage]:
+		imgList: list[tk.PhotoImage] = []
+		for filename in self._sprList:
+			imgList.append(self._sprImgDict[filename])
+		return imgList
+
+	def GetPtList(self) -> list:
+		return self._ptList
 
 	def SelectEntity(self, entityName: str) -> None:
 		if entityName != self._selectedEntity:
-			self._selectedEntity = entityName
+			self._selectedEntity = entityName # set the newly selected entity
+			self._selectedAnim = "" # deselect whatever animation we had previously selected
+			self._selectedSpr = "" # deselect whatever sprite we had previously selected
+			# TODO: deselect whatever point we had previously selected
 			self._animList = [] # clear the animation list before re-generating it
 
-			# generate a list of animations for the current entity
+			# point data is stored by entity, so we can load point data once the entity has been selected
+			if self._pointData is not None:
+				assert self._pointData.HasUnsavedChanges() == False # TODO: raise a popup warning about unsaved changes (instead of having this assert)
 			entityPath: str = os.path.join(jns.READ_LOCATION_TEXTURES_ENTITIES, entityName)
+			self._pointData = PointData(entityPath)
+
+			# generate a list of animations for the current entity
 			entityFilenames: list[str] = jns.GetFilesWithConvention(entityPath, jns.CONVENTION_SPR_ENTITY)
 
 			for entityFilename in entityFilenames:
 				# get the animation name from the filename
 				animName: str = entityFilename.split("_")[0] # TODO: come up with a better way to get the animation name from the filename
-				if animName not in self._selectionOptions:
-				# only add unique animation names to the selection options list
+				# only add unique animation names to the animation list
+				if animName not in self._animList:
 					self._animList.append(animName)
 
-	
-class EntitySelectorModel:
+	def SelectAnim(self, animName: str) -> None:
+		if animName != self._selectedAnim:
+			self._selectedAnim = animName # set the newly selected animation
+			self._selectedSpr = "" # deselect whatever sprite we had previously selected
+			# TODO: deselect whatever point we had previously selected
+			self._sprList = [] # clear the sprite list before re-generating it
+			# NOTE: we do NOT clear out self._sprImgDict because we may want to access the images multiple times
 
-	def __init__(self, basePath: str) -> None:
-		self._entityList: list[str] = jns.GetFoldersAtPath(basePath)
-
-	def GetEntityList(self) -> list[str]:
-		return self._entityList
-	
-class AnimationSelectorModel:
-
-	def __init__(self) -> None:
-		self._selectedEntity: str = ""
-		self._selectionOptions: list[str] = []
-		
-
-	def GetSelectionOptions(self, entityName: str) -> list[str]:
-		if entityName != self._selectedEntity:
-			# clear the list before re-generating it
-			self._selectedEntity = entityName
-			self._selectionOptions = []
-
-			# generate a list of animations for the current entity
-			entityPath: str = os.path.join(jns.READ_LOCATION_TEXTURES_ENTITIES, entityName)
+			# generate a list of sprite filenames for the current animation
+			entityPath: str = os.path.join(jns.READ_LOCATION_TEXTURES_ENTITIES, self._selectedEntity)
 			entityFilenames: list[str] = jns.GetFilesWithConvention(entityPath, jns.CONVENTION_SPR_ENTITY)
 
 			for entityFilename in entityFilenames:
-				# get the animation name from the filename
-				animName: str = entityFilename.split("_")[0] # TODO: come up with a better way to get the animation name from the filename
-				if animName not in self._selectionOptions:
-				# only add unique animation names to the selection options list
-					self._selectionOptions.append(animName)
+				# only add the filename to the sprite list if the animation name is found in the filename
+				if animName in entityFilename:
+					self._sprList.append(entityFilename)
+					# only add an image to the dict if it has not already been loaded
+					if entityFilename not in self._sprImgDict.keys():
+						imgPath: str = os.path.join(entityPath, entityFilename)
+						self._sprImgDict[entityFilename] = tk.PhotoImage(file=imgPath, name=entityFilename)
+	
+	def SelectSpr(self, sprFilename: str) -> None:
+		if sprFilename != self._selectedSpr:
+			self._selectedSpr = sprFilename # set the newly selected sprite
+			# TODO: deselect whatever point we had previously selected
+			self._ptList = self._pointData.GetPointsForSprite(self._selectedSpr) # the PointData class will generate this list for us. easy!
 
-		return self._selectionOptions
+	def AddPoint(self, ptName: str) -> None:
+		self._pointData.AddPt(self._selectedSpr, ptName)
+
+class Point:
+
+	KEY_X: str = "x"
+	KEY_Y: str = "y"
+
+	def __init__(self, name: str, x: float | None = None, y: float | None = None) -> None:
+		self.name: str = name
+		self.x: float | None = x
+		self.y: float | None = y
+
+	def GetDict(self) -> dict[str, dict[str, float | None]]:
+		return { self.name: { Point.KEY_X: self.x, Point.KEY_Y: self.y } }
+
+class PointData:
+
+	FILE_NAME: str = "points.json"
+
+	POINT_NAMES_KEY: str = "pointNames"
+	SPRITES_KEY: str = "sprites"
+
+	def __init__(self, entityPath: str) -> None:
+		self._filePath: str = os.path.join(entityPath, PointData.FILE_NAME)
+		self._unsavedChanges: bool = False
+		#            sprFilename   pointName
+		#                  vvv       vvv
+		self._points: dict[str, dict[str, Point]] = {}
+		
+		if os.path.isfile(self._filePath):
+			# load a json file and create points
+			print(f"Loading existing point data from {self._filePath}")
+			self._LoadJsonData()
+		else:
+			# create empty point data
+			print(f"No existing point data to load from {self._filePath}")
+			self._CreateNewData(entityPath)
+			self._unsavedChanges = True
+
+	def HasUnsavedChanges(self) -> bool:
+		return self._unsavedChanges
+
+	def GetPointsForSprite(self, sprFilename: str) -> list[Point]:
+		assert sprFilename in self._points.keys()
+		return list(self._points[sprFilename].values())
+	
+	def AddPt(self, sprFilename: str, ptName: str) -> None:
+		assert sprFilename in self._points.keys()
+		assert ptName not in self._points[sprFilename].keys()
+		self._points[sprFilename][ptName] = Point(ptName)
+
+	def _CreateNewData(self, entityPath: str) -> None:
+		assert len(self._points.keys()) == 0 # self._points MUST BE EMPTY when this function is called
+		sprFilenames: list[str] = jns.GetFilesWithConvention(entityPath, jns.CONVENTION_SPR_ENTITY)
+		for sprFilename in sprFilenames:
+			self._points[sprFilename] = {}
+
+	def _LoadJsonData(self) -> None:
+		with open(self._filePath, 'r') as jsonFile:
+			loadedData: dict = json.load(jsonFile)
+		pointNameList: list[str] = loadedData[PointData.POINT_NAMES_KEY]
+		print(pointNameList)
+
+	def _SaveJsonData(self) -> None:
+		pass
