de798da1eb239af5a885c6510da320d6c5ce8ec3
HankDeTank05 <henryhankholman@gmail.com>
2025-02-03 10:52:49 -0600

    Update levelEditor_view_tilePalette.py

--- tools/levelEditor_view_tilePalette.py
+++ tools/levelEditor_view_tilePalette.py
@@ -8,5 +8,6 @@ import jnscommon as jns
 class TilePaletteView:
 
 	def __init__(self, parent: tk.Tk, column: int, row: int, columnspan: int = 1, rowspan: int = 1, sticky: str = "NSEW") -> None:
-		# TODO: (henry) tile palette gui code goes here
 		pass
+
+	# TODO: (henry) tile palette gui code
